2d8fc42500e6847c70565455954d691323c01e8f
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-15 16:07:48 +0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Marat Fakhrizhanov on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
