07f0d35e71765ade3561216b15f0c27bf1db26e5
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-16 21:09:18 +0300

    add date in model

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -6,16 +6,20 @@
 //
 
 import SwiftData
+import Foundation
 
 @Model
 class Prospect {
     var name: String
     var emailAddress: String
     var isContacted: Bool
+    var date: Date
     
-    init(name: String, emailAddress: String, isContacted: Bool) {
+    init(name: String, emailAddress: String, isContacted: Bool, date: Date) {
         self.name = name
         self.emailAddress = emailAddress
         self.isContacted = isContacted
+        self.date = date
     }
+    
 }
