7ebb86ac9b8c55629f4fa3ec3fdbadedf67628e2
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-16 14:49:38 +0300

    project

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,8 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Marat Fakhrizhanov on 15.11.2024.
+//
+
+import Foundation
---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Marat Fakhrizhanov on 15.11.2024.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
